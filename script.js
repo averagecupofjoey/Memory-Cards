bde4cea513e315e1299c09253d65409bd5c2a5ff
averagecupofjoey <52182654+averagecupofjoey@users.noreply.github.com>
2020-08-03 11:55:21 -0400

    Added a "learned card" deck option
    This is a separate "deck" of data locally stored and accessed to help improve the study capabilities of the app

--- script.js
+++ script.js
@@ -11,15 +11,20 @@ const clearBtn = document.getElementById("clear");
 const addContainer = document.getElementById("add-container");
 const deleteBtn = document.getElementById("deleteCard");
 const learnBtn = document.getElementById("learnCard");
+const showLearnBtn = document.getElementById("showLearned");
+const showUnlearnedBtn = document.getElementById("showUnlearned");
 
 // Keep track of current card
 let currentActiveCard = 0;
 
 // Store DOM cards
-const cardsEl = [];
+let cardsEl = [];
 
-// Store card data
+// Store unlearnedcard data
 let cardsData = getCardsData();
+
+// Store learned data
+let learnedCardsData = getLearnedCardsData();
 
 // const cardsData = [
 //   {
@@ -36,9 +41,14 @@ let cardsData = getCardsData();
 //   },
 // ];
 
-// Create all cards
+// Create unlearned cards
 function createCards() {
   cardsData.forEach((data, index) => createCard(data, index));
+}
+
+// Create learned cards
+function createLearnedCards() {
+  learnedCardsData.forEach((data, index) => createCard(data, index));
 }
 
 // Create a single card in DOM
@@ -80,23 +90,28 @@ function updateCurrentText() {
   currentEl.innerText = `${currentActiveCard + 1}/${cardsEl.length}`;
 }
 
-// Get cards from local storage
+// Get unlearned cards from local storage
 function getCardsData() {
-  const cards = JSON.parse(localStorage.getItem("cards"));
+  const cards = JSON.parse(localStorage.getItem("unlearnedCards"));
   return cards === null ? [] : cards;
 }
 
-// Add card to local storage
+// Get LEARNED cards from local storage
+function getLearnedCardsData() {
+  const cards = JSON.parse(localStorage.getItem("learnedCards"));
+  return cards === null ? [] : cards;
+}
+
+// Add unlearnedcard to local storage
 function setCardsData(cards) {
-  localStorage.setItem("cards", JSON.stringify(cards));
+  localStorage.setItem("unlearnedCards", JSON.stringify(cards));
   window.location.reload();
 }
 
-// Delete current card from local storage
-function deleteCurrentCard() {
-  let newData = cardsData.splice(currentActiveCard - 1, 1);
-  cardsData = newData;
-  setCardsData(cardsData);
+// Add learned card to local storage
+function setLearnedData(cards) {
+  localStorage.setItem("learnedCards", JSON.stringify(cards));
+  //  window.location.reload();  the delete cards function will reload window
 }
 
 createCards();
@@ -157,7 +172,6 @@ addCardBtn.addEventListener("click", () => {
     cardsData.push(newCard);
     setCardsData(cardsData);
   }
-  console.log(question, answer);
 });
 
 //clear cards button
@@ -173,4 +187,42 @@ deleteBtn.addEventListener("click", () => {
 });
 
 // adjustments
-let currentDeck = unlearned;
+
+// Delete current card from local storage
+function deleteCurrentCard() {
+  cardsData.splice(currentActiveCard, 1);
+  setCardsData(cardsData);
+}
+
+// Move cards to learned deck
+function learnCurrentCard() {
+  // store current card
+  let learnedCard = cardsData.slice(currentActiveCard, currentActiveCard + 1);
+  learnedCardsData.push(learnedCard[0]);
+  // set in learned local storage
+  setLearnedData(learnedCardsData);
+
+  // delete in unlearned
+  deleteCurrentCard();
+}
+
+// Learn button event listener
+learnBtn.addEventListener("click", () => learnCurrentCard());
+
+// Show learned cards event listener
+showLearnBtn.addEventListener("click", () => {
+  cardsEl = [];
+  cardsContainer.innerHTML = "";
+  createLearnedCards();
+  currentActiveCard = 0;
+  updateCurrentText();
+});
+
+// Show unlearned cards event listener
+showUnlearnedBtn.addEventListener("click", () => {
+  cardsEl = [];
+  cardsContainer.innerHTML = "";
+  createCards();
+  currentActiveCard = 0;
+  updateCurrentText();
+});
